0b8d5e9d3fac19086d9670bcff4454e7c17cafaf
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2022-06-14 18:25:58 +0300

    Add files via upload

--- gift_bot/settings.py
+++ gift_bot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pn#or9b@32%a86#8xc$h-$y-sfciez8!lokadl3@5n3**(zr#a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['193.168.3.79', '127.0.0.1']
 
 
 # Application definition
@@ -120,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
