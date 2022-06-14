966bd60fc9fab7d719bab1acae4ba7ceff6f422c
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2022-06-14 18:27:44 +0300

    Add files via upload

--- gift_bot/settings.py
+++ gift_bot/settings.py
@@ -122,9 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 STATIC_ROOT = "static/"
 
 # Default primary key field type
